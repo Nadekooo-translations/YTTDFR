c143b01dcb5a9d501617fff00c2dfd24423aa4df
vinceh121 <contact@vinceh121.me>
2023-09-15 00:18:57 +0200

    feat: assets encrypt script

---
+++ tools/crypt.js
@@ -0,0 +1,54 @@
+const path = require("node:path");
+const { readFileSync, writeFileSync, readdirSync, statSync, copyFileSync, mkdirSync } = require("node:fs");
+const { Encrypter } = require("rpgmaker-mv-crypt");
+
+const ignoreCrypt = new Set([path.join("img", "system", "Loading.png")]);
+
+const enc = new Encrypter("d41d8cd98f00b204e9800998ecf8427e");
+
+const distPath = (pngPath) => {
+	const encPathComponents = path.parse(path.join("yttd", "www", pngPath));
+	return path.join(encPathComponents.dir, "FR", encPathComponents.name + ".rpgmvp");
+};
+
+const encrypt = (pngPath) => {
+	const pngBuf = readFileSync(pngPath);
+	const encPath = distPath(pngPath);
+
+	console.log("Writing", pngPath, "to", encPath);
+
+	const encFile = enc.encrypt(pngBuf);
+
+	try {
+		mkdirSync(path.dirname(encPath));
+	} catch (e) { } // ignore "already exists" errors
+
+	writeFileSync(encPath, encFile);
+};
+
+const move = (pngPath) => {
+	const outPath = path.join("yttd", "www", pngPath);
+
+	console.log("Copying", pngPath, "to", outPath);
+
+	copyFileSync(pngPath, outPath);
+};
+
+const recurse = (folder) => {
+	const list = readdirSync(folder);
+
+	for (const child of list) {
+		const childPath = path.join(folder, child);
+		const stat = statSync(childPath);
+
+		if (stat.isDirectory()) {
+			recurse(childPath);
+		} else if (!ignoreCrypt.has(childPath)) {
+			encrypt(childPath);
+		} else {
+			move(childPath);
+		}
+	}
+};
+
+recurse("img");
