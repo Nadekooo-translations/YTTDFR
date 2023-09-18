c7dcbcd538b508e7b0005963c79e22314fa36fb2
vinceh121 <contact@vinceh121.me>
2023-09-18 14:10:52 +0200

    fix: mitigate None bug of Weblate

--- tools/compress.js
+++ tools/compress.js
@@ -1,5 +1,29 @@
 const fs = require("node:fs");
 const { compressToBase64 } = require("lz-string");
 
-fs.writeFileSync("FR.cte", compressToBase64(JSON.stringify(JSON.parse(String(fs.readFileSync("fr.json"))))))
+const fr = JSON.parse(String(fs.readFileSync("fr.json")));
+
+const recurseNoneFix = (obj) => {
+	if (Array.isArray(obj)) {
+		for (let i = 0; i < obj.length; i++) {
+			if (obj[i] === "None") {
+				obj[i] = null;
+			} else {
+				recurseNoneFix(obj[i]);
+			}
+		}
+	} else if (typeof obj === "object") {
+		for (const key in obj) {
+			if (obj[key] === "None") {
+				obj[key] = null;
+			} else {
+				recurseNoneFix(obj[key]);
+			}
+		}
+	}
+};
+
+recurseNoneFix(fr);
+
+fs.writeFileSync("FR.cte", compressToBase64(JSON.stringify(fr)))
 
