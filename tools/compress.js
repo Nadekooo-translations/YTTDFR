0f059b1ebb71bacc520e1ccab19dfca71abdb4d9
vinceh121 <contact@vinceh121.me>
2024-07-28 19:15:39 +0200

    fix: remove workaround for WeblateOrg/weblate#9963

--- tools/compress.js
+++ tools/compress.js
@@ -3,27 +3,5 @@ const { compressToBase64 } = require("lz-string");
 
 const fr = JSON.parse(String(fs.readFileSync("fr.json")));
 
-const recurseNoneFix = (obj) => {
-	if (Array.isArray(obj)) {
-		for (let i = 0; i < obj.length; i++) {
-			if (obj[i] === "None") {
-				obj[i] = null;
-			} else {
-				recurseNoneFix(obj[i]);
-			}
-		}
-	} else if (typeof obj === "object") {
-		for (const key in obj) {
-			if (obj[key] === "None") {
-				obj[key] = null;
-			} else {
-				recurseNoneFix(obj[key]);
-			}
-		}
-	}
-};
-
-recurseNoneFix(fr);
-
 fs.writeFileSync("FR.cte", compressToBase64(JSON.stringify(fr)))
 
