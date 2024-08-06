5cc8ae1fdfd867b566a228975278fb20dbb6b709
vinceh121 <contact@vinceh121.me>
2024-08-06 21:20:10 +0200

    fix: no argument handleing in apply-animation-patch

--- tools/apply-animation-patch.mjs
+++ tools/apply-animation-patch.mjs
@@ -1,12 +1,6 @@
 import * as jsondiffpatch from "jsondiffpatch";
 import {readFileSync, writeFileSync} from "node:fs";
 
-if (argv.length != 2) {
-	console.error("Usage: apply-animation-patch.js");
-	console.error("Output will be written in-place to yttd/www/data/Animations.json");
-	exit(-1);
-}
-
 const path = "yttd/www/data/Animations.json";
 
 const source = JSON.parse(String(readFileSync(path)));
