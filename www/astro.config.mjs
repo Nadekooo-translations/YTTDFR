5c75f3d90f748016f109fba80f67b6e10b102578
vinceh121 <contact@vinceh121.me>
2024-09-15 11:46:10 +0200

    feat: default prefetch mode

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -5,5 +5,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: "https://yttd.fr",
   integrations: [sitemap()],
-  prefetch: true,
+  prefetch: {
+    defaultStrategy: "viewport",
+  },
 });
