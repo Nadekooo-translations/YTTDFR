6e933ba1682ff133dba0164587bb8d44e81b46bb
vinceh121 <contact@vinceh121.me>
2024-10-25 00:06:56 +0200

    feat: save title editing and download

--- www/src/components/vue/SaveUtils.ts
+++ www/src/components/vue/SaveUtils.ts
@@ -1,6 +1,6 @@
 import lz from "lz-string";
 
-interface Save {
+export interface Save {
 	/**
 	 * Base64 string as is in the localStorage
 	 */
@@ -8,16 +8,33 @@ interface Save {
 	data: object;
 }
 
-type SaveData = Record<number, Save>;
+export type SaveData = Record<number, Save>;
+
+export interface SaveEntry {
+	/**
+	 * Always "RPGMV"
+	 */
+	globalId: string;
+	title: string;
+	characters: Array<any>;
+	faces: Array<any>;
+	playtime: string;
+	timestamp: number;
+	mapName: string;
+}
+
+export type SaveIndex = Array<SaveEntry | null>;
+
+export const getRawSave = (idx: number): string | null => {
+	return localStorage.getItem(`Kimi RPG File${idx}`);
+};
 
 export const readSaveData = (): SaveData => {
 	const data: SaveData = {};
 
 	for (let i = 1; i <= 100; i++) {
-		const key = `Kimi RPG File${i}`;
-		
 		let raw;
-		if (raw = localStorage.getItem(key)) {
+		if (raw = getRawSave(i)) {
 			const save = JSON.parse(lz.decompressFromBase64(raw));
 
 			data[i] = { raw, data: save };
@@ -27,14 +44,19 @@ export const readSaveData = (): SaveData => {
 	return data;
 };
 
-export const readSaveIndex = () => {
+export const readSaveIndex = (): SaveIndex => {
 	const raw = localStorage.getItem("Kimi RPG Global");
 
 	if (!raw) {
-		return;
+		return [];
 	}
 
 	const data = JSON.parse(lz.decompressFromBase64(raw));
 
 	return data;
+};
+
+export const writeSaveIndex = (index: SaveIndex) => {
+	const b64 = lz.compressToBase64(JSON.stringify(index));
+	localStorage.setItem("Kimi RPG Global", b64);
 };
