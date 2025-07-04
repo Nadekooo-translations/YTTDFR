9f9e1e65deac1c8ae5cbb94801bc8cff0a960232
Vincent <vince@iwit-systems.fr>
2025-07-04 13:43:59 +0200

    feat: base blog

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -3,10 +3,12 @@ import sitemap from '@astrojs/sitemap';
 
 import vue from "@astrojs/vue";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://yttd.fr",
-  integrations: [sitemap(), vue({devtools: true})],
+  integrations: [sitemap(), vue({devtools: true}), mdx()],
   prefetch: {
     defaultStrategy: "viewport",
     prefetchAll: true,
